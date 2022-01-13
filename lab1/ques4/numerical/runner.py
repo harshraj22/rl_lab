Fixed numerical method
Total prob = sum of prob of reaching in i'th state, i ranges from 0 to inf

lab1/ques4/numerical/runner.py
@@ -1,15 +1,25 @@
 import numpy as np
 from data_loader.transition_probs import get_transition_probs
+from tqdm import tqdm
+import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+np.set_printoptions(linewidth=np.inf)
 
 if __name__ == '__main__':
     initial_prob = np.zeros(9)
     initial_prob[0] = 1
+    probs_sum = 0
 
     transition_probs = get_transition_probs()
-    P_8 = np.linalg.matrix_power(transition_probs, 8)
+    current_prob = np.identity(n = 9, like=transition_probs)
+    INFINITY = 10**7
 
-    final_probs = initial_prob.T @ P_8
-    print(final_probs)
+    for i in tqdm(range(INFINITY), desc='Summing prob of ending at 8th state in iteration'):
+        current_prob = current_prob @ transition_probs
+        probs_sum += (initial_prob @ current_prob)[8]
 
-    print(f'sum of probabilities of all states: {final_probs.sum()}')
+    print(f'Probability of ending at state 8 is: {probs_sum}')
+