dc2860eadbaf1e81902672b089fb56d1fe0b8605
Harsh Raj <harshraj22aug@gmail.com>
2022-01-13 17:35:59 +0530

    Fixed simulation

--- lab1/ques4/simulation/data_loader/environments.py
+++ lab1/ques4/simulation/data_loader/environments.py
@@ -6,9 +6,9 @@ import numpy as np
 
 class SnakeAndLadderEnv(gym.Env):
     """Gym Environment to simulate the snake and ladder game"""
-    def __init__(self, current_state=0, num_states=9, dead_states=(2, 4), view=None) -> None:
+    def __init__(self, current_state=0, num_states=9, dead_states=(2, 4), view=None, max_timesteps=40) -> None:
         """Initialize the environment. The player starts at position 'current_state',
-        and takes actions in range(1, 3). The game ends when the player reaches 
+        and takes actions in range(1, 6). The game ends when the player reaches 
         either the 'dead_states' (in which case he/she looses and gets a reward
         of 0) or when he/she reaches the 'num_states-1' postion (in which case
         he/she wins and gets a reward of 1).
@@ -21,25 +21,33 @@ class SnakeAndLadderEnv(gym.Env):
               in which the environment should be printed. Note that the 'num_states'
               should be equal to the product of elements of 'view'. If not provided,
               it is assumed to be of shape (num_states,).
+            max_timesteps (int, optional): the maximum number of timesteps per game.
+              This is used to exit the game with 0 reward in case the player ends up in
+              a dead state. Defaults to 40.
         """
         super().__init__()
         self.current_state = current_state
         self.num_states = num_states
         self.dead_states = dead_states
         self.view = view if view else (self.num_states, )
+        self.max_timesteps = max_timesteps
+        self.current_timestep = 0
 
     def reset(self) -> int:
         self.current_state = 0
+        self.current_timestep = 0
         return self.current_state
 
     def step(self, action: int) -> (int, float, bool, dict):
+        """Take an action and return the next state, reward, done and info."""
+        self.current_timestep += 1
         # you can not step out of the board by taking an action. Such actions would be ignored.
-        if self.current_state + action < self.num_states:
+        if self.current_state + action < self.num_states and action in [1, 2]:
             self.current_state += action
         return (
             self.current_state,
             int(self.current_state == self.num_states - 1),
-            bool(self.current_state in [self.num_states-1, *self.dead_states]),
+            bool(self.current_state in [self.num_states-1, *self.dead_states]) or (self.current_timestep == self.max_timesteps),
             {}
         )
 
@@ -47,8 +55,8 @@ class SnakeAndLadderEnv(gym.Env):
         states = np.chararray(shape=(self.num_states,), unicode=True)
         for i in range(self.num_states):
             states[i] = str(i)
-        states[self.current_state] = '#'
         states[list(self.dead_states)] = '_'
+        states[self.current_state] = '#'
         print(states.reshape(self.view))
         print('#: current state \n_: dead state\n')
 
--- lab1/ques4/simulation/runner.py
+++ lab1/ques4/simulation/runner.py
@@ -2,26 +2,60 @@ import numpy as np
 import hydra
 import pathlib
 from data_loader.environments import SnakeAndLadderEnv
+from tqdm import tqdm
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+
+def play_a_game(max_timesteps: int = 40) -> int:
+    """Play a single game of Snake and Ladder.
+
+    Args:
+        max_timesteps (int, optional): Number of timesteps for which a single game
+          lasts. Defaults to 40.
+
+    Returns:
+        int: The reward of the game.
+    """
+    env = SnakeAndLadderEnv(view=(3, 3))
+    obs, done = env.reset(), False
+    reward = 0
+
+    # play a game
+    while not done:
+        action = np.random.randint(1, 7)
+        logger.debug(f'Action taken: {action}')
+        obs, reward, done, _ = env.step(action)
+        # env.render()
+    logger.info(f'Reward: {reward}')
+    env.close()
+    return reward
+
+
+def simulate(num_games: int, max_timestep_per_game: int) -> None:
+    """Simulate the experiment of playing a series of games, and finding the
+    probability of winning using the rewards returned.
+
+    Args:
+        num_games (int): Number of games to simulate.
+        max_timestep_per_game (int): Maximum number of timesteps per game. This 
+            is used to exit the game with 0 reward in case the player ends up in
+            a dead state.
+    """
+    rewards = []
+    for _ in tqdm(range(num_games), desc='Simulating games'):
+        rewards.append(play_a_game(max_timesteps=max_timestep_per_game))
+    logger.debug(rewards)
+    print(f'Probability of winning: {np.mean(rewards)}')
 
 
 @hydra.main(config_path="conf", config_name="configs")
 def main(cfg):
-    # np.random.seed(cfg.seed)
-    env = SnakeAndLadderEnv(view=(3, 3))
-    obs, done = env.reset(), False
-    rewards = []
-
-    # play a game
-    while not done:
-        action = np.random.randint(1, 3)
-        print(f'Action taken: {action}')
-        obs, reward, done, _ = env.step(action)
-        rewards.append(reward)
-        env.render()
-    print(f'Reward: {np.sum(rewards)}')
-    env.close()
+    pathlib.Path(f'{pathlib.Path.cwd()}/{cfg.exp.data_dir}/').mkdir(parents=True, exist_ok=True)
+    simulate(num_games=cfg.exp.num_games, max_timestep_per_game=cfg.exp.max_timestep_per_game)
 
 
 if __name__ == '__main__':
-    pathlib.Path(f'{pathlib.Path.cwd()}/figs/').mkdir(parents=True, exist_ok=True)
     main()
