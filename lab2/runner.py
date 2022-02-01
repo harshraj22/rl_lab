74b690c86b598fff40fc6c0e5a3a191398acb870
Harsh Raj <harshraj22aug@gmail.com>
2022-02-02 01:30:19 +0530

    config += 1

--- lab2/runner.py
+++ lab2/runner.py
@@ -32,7 +32,7 @@ def main(cfg):
         )
 
     if cfg.agent.type == 'eps_greedy':
-        agent = EpsilonGreedyAgent(0.3, env.num_arms, initial_temp=1.0, decay_factor=1.001)
+        agent = EpsilonGreedyAgent(cfg.agent.eps_greedy.epsilon, env.num_arms, initial_temp=cfg.agent.eps_greedy.initial_temp, decay_factor=cfg.agent.eps_greedy.decay_factor)
     elif cfg.agent.type == 'softmax':
         agent = SoftmaxAgent(env.num_arms)
     elif cfg.agent.type == 'ucb':
@@ -40,7 +40,7 @@ def main(cfg):
     elif cfg.agent.type == 'thompson_sampling':
         agent = ThompsonSamplingAgent(env.num_arms, underlying_dist=cfg.env.reward_dist)
     elif cfg.agent.type == 'reinforce':
-        agent = ReinforceAgent(env.num_arms, baseline=cfg.agent.reinforce.baseline, beta=0.2, alpha=0.9)
+        agent = ReinforceAgent(env.num_arms, baseline=cfg.agent.reinforce.baseline, beta=cfg.agent.reinforce.beta, alpha=cfg.agent.reinforce.alpha)
     else:
         raise ValueError(f'Unknown agent type: {cfg.agent.type}. Please choose from: eps_greedy, softmax, ucb, thompson_sampling, reinforce')
 
@@ -63,3 +63,5 @@ if __name__ == '__main__':
     main()
     # print(wandb.)
     # print(logger.handlers)
+
+    # python3 runner.py -m agent.type=reinforce env.reward_dist=gaussian seed=32,12,23,43,54,65,76,234,2342,3423,42342,34,234,234,234,23 wandb_tracking=disabled agent.reinforce.baseline=False total_timesteps=5000 env.num_arms=5
