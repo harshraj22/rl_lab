54be19c4b05a0b83b4ad29712831d261e9792ac1
Harsh Raj <harshraj22aug@gmail.com>
2022-02-01 12:57:01 +0530

    fixed reinforce

--- lab2/models/reinforce.py
+++ lab2/models/reinforce.py
@@ -17,16 +17,25 @@ class ReinforceAgent(MultiArmBanditAgent):
         self.num_arms = num_arms
         self.baseline = baseline
         self.running_means = [RunningMeanReinforce(baseline=baseline) for _ in range(num_arms)]
+        self._baseline_rewards = 0.0
+        self._timestep = 0
+
+    @property
+    def average_reward(self):
+        if self._timestep == 0:
+            return 0.0
+        return self._baseline_rewards / self._timestep
 
     def update_mean(self, arm_index: int, reward: int) -> None:
-        self.running_means[arm_index].update_preference(reward)
-        self.running_means[arm_index].update_mean(reward)
+        self.running_means[arm_index].update_preference(reward, self.average_reward)
+        self.running_means[arm_index].update_mean(reward, self.average_reward)
 
     def forward(self, state: int) -> int:
         """Choose the arm whose probability is maximum. The probability is calculated
         by taking softmax of the preferences of all arms."""
         pi = softmax([mean.preference for mean in self.running_means])
         # logger.info(f'pi: {pi}, sum: {np.sum(pi)}')
+        self._timestep += 1
         return np.random.choice(self.num_arms, 1, p=pi)[0]
 
     def __call__(self, state: int) -> int:
