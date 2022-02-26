35c4e24b1f0b063fc0f2b386de5f35f7942f5095
akhileshkb <akhilesh31march@gmail.com>
2022-02-26 16:51:55 +0530

    env step complete

--- lab3/data_loader/environments.py
+++ lab3/data_loader/environments.py
@@ -2,10 +2,22 @@ import gym
 from gym.spaces import Discrete
 from typing import Tuple
 import sys
+import numpy as np
 
 
 sys.path.insert(0, '../')
 from base.iteration_env import IterationEnv
+
+from enum import IntEnum
+
+
+class Directions(IntEnum):
+    UP = 0
+    LEFT = 1
+    DOWN = 2
+    RIGHT = 3
+
+    TOTAL_DIRECTIONS = 4
 
 
 class GridWorldEnvironment(IterationEnv):
@@ -27,14 +39,18 @@ class GridWorldEnvironment(IterationEnv):
     state in East direction and 10% chance of moving to the state in West direction.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, start_state: int = 0) -> None:
         super(GridWorldEnvironment, self).__init__()
         self.action_space = Discrete(4)
         self.observation_space = Discrete(15)
-        self._state = 0
+        self._state = start_state
+        self._goal_state = 3
+        self._terminal_state = 7
+        self._goal_state_reward = 1
+        self._terminal_state_reward = -100
 
-    def reset(self) -> None:
-        self._state = 0
+    def reset(self, start_state: int = 0) -> None:
+        self._state = start_state
 
     def render(self) -> None:
         pass
@@ -59,7 +75,37 @@ class GridWorldEnvironment(IterationEnv):
 
     def step(self, action: int) -> Tuple[int, float, bool, dict]:
         """Return observation, reward, done, and info."""
-        pass
+        x, y = self.integer_state_to_coordinates(self._state)
+
+        transition_probability = np.random.uniform(0, 1)
+        if 0.8 <= transition_probability < 0.9:
+            action = (action - 1 + Directions.TOTAL_DIRECTIONS) % Directions.TOTAL_DIRECTIONS
+        elif transition_probability >= 0.9:
+            action = (action + 1 + Directions.TOTAL_DIRECTIONS) % Directions.TOTAL_DIRECTIONS
+
+        if action == Directions.UP:
+            y = y - 1
+        elif action == Directions.DOWN:
+            y = y + 1
+        elif action == Directions.RIGHT:
+            x = x + 1
+        elif action == Directions.LEFT:
+            x = x - 1
+        else:
+            raise ValueError("Illegal action")
+
+        if self.is_valid_coordinate(x, y):
+            self._state = self.coordinates_to_integer_state(x, y)
+
+        reward, done = 0, 0
+        if self._state == self._goal_state:
+            reward = self._goal_state_reward
+            done = 1
+        elif self._state == self._terminal_state:
+            reward = self._terminal_state_reward
+            done = 1
+        
+        return self._state, reward, done, {}        
 
     @property
     def state(self) -> int:
@@ -70,3 +116,13 @@ class GridWorldEnvironment(IterationEnv):
     def state(self, new_state: int) -> None:
         """Set the current state of the environment."""
         self._state = new_state
+
+
+if __name__ == '__main__':
+    env = GridWorldEnvironment()
+    done = False
+    while not done:
+        action = env.action_space.sample()
+        print(action)
+        obs, reward, done, _ = env.step(action)
+        print(obs, reward, done)
