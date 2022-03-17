dd05232902a4f00890120f24b1fef131d3a302cb
Harsh Raj <harshraj22aug@gmail.com>
2022-03-17 13:21:31 +0530

    Added first visit monte carlo (on policy). Not tested

---
+++ lab4/models/on_policy_mc.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+from typing import DefaultDict, List, Tuple
+import numpy as np
+
+import sys
+sys.path.append('..')
+
+from base.baseagent import BaseAgent
+
+
+class FirstVisitMonteCarlo(BaseAgent):
+    """Agent that uses first-visit Monte Carlo (on-policy MC)
+    https://i.stack.imgur.com/033M8.png
+    """
+    def __init__(self, num_states: int, num_actions: int, eps: float = 0.2) -> None:
+        super(FirstVisitMonteCarlo, self).__init__()
+        self.num_states = num_states
+        self.num_actions = num_actions
+        self.eps = eps
+
+        # initialize the policy
+        self.policy = np.random.randint(0, self.num_actions, size=self.num_states)
+
+        # initialize the Q-values
+        self.Q = np.random.randn(self.num_states, self.num_actions)
+
+        # Returns
+        self.returns: DefaultDict[Tuple[int, int], List[int]] = defaultdict(list)
+
+    def forward(self, state: int) -> int:
+        """Select an action."""
+        return self.policy[state]
+
+    def learn(self) -> None:
+        """Update the policy"""
+        for state in range(self.num_states):
+            for action in range(self.num_actions):
+                self.Q[state][action] = np.mean(self.returns[(state, action)])
+
+        # update the policy
+        for state in range(self.num_states):
+            self.policy[state] = np.argmax(self.Q[state])
+
+    def step(self, state: int, action: int, reward: int) -> None:
+        """Update the agent's knowledge.
+
+        Parameters
+        ----------
+        state : int
+            The current state of the environment.
+        action : int
+            The action selected by the agent.
+        reward : int
+            The reward received from the environment.
+        """
+        # update the returns
+        if (state, action) in self.returns.keys():
+            pass # skip if the state-action pair has been visited before
+        self.returns[(state, action)].append(reward)
